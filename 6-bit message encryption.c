35c4676e45b669459d968bc50271689b5cd02727
Shoaib-max <78849008+Shoaib-max@users.noreply.github.com>
2022-07-03 12:30:50 +0530

    Update 6-bit message encryption.c

--- 6-bit message encryption.c
+++ 6-bit message encryption.c
@@ -19,12 +19,18 @@ char str[max_msg];
 char *p = str;
 char arr[40];
 int main(){
-	int i=0;
-
-	    for( i=10; i<36 ;i++){
-	     arr[i]='a'+i-10;  
-		 }
-		 arr[36]=' ';
+ 
+ int i;
+	    for( i=10; i<=68 ;i++){
+	     arr[i]='A'+i-10;       }
+	for( i=0; i<10 ;i++){
+	    arr[i]='0'+i;       }
+		arr[69]=' ';
+		arr[70]=',';
+		for(i=71;i<=86;i++)
+		arr[i]='!'+1;
+		arr[87]='@';
+ 	     
 	starter();
 	while(1)
 	{
@@ -86,7 +92,7 @@ void encryption(){
 		p++;
 	}
 	count--;
-	printf("\n\tTHE KEY OF ENCRYPTED MESSAGE IS = %d  ",count*6);
+	printf("\n\tTHE KEY OF ENCRYPTED MESSAGE IS = %d  ",count*7);
 	printf("\n");
 	printf(" \n");
 	system("pause");
@@ -104,18 +110,29 @@ void ecryptioninput(){
 }
 void encrypt(char par){
 		int i,up,down,temp;
-		int array[10];
+		int array[1000];
 	    char lettr = par;
 	   
-	    for(i=10;i<37;i++)
+	    for(i=0;i<=87;i++)
 	    {
 	    	temp=i;
 			 if(lettr == arr[i]){	 
 				   up=0;
+				
+				   if(temp<1&&temp>=0)
+				printf(" 0  0  0  0  0  0  0 ");
+				    if(temp<2&&temp>=1)
+				printf(" 0  0  0  0  0  0 ");
+				   if(temp<4&&temp>=2)
+				printf(" 0  0  0  0  0 ");
+				 if(temp<8&&temp>=4)
+				printf(" 0  0  0  0 ");
+				if(temp>=8&&temp<16)
+				printf(" 0  0  0 ");
 			 if(temp<32&&temp>=16)
-				printf(" 0 ");
-			 if(temp>=10&&temp<16)
 				printf(" 0  0 ");
+				 if(temp>=32&&temp<64)
+				printf(" 0 ");
 				while(temp>0)
 				{ 
 					array[up] = temp % 2;
@@ -125,6 +142,9 @@ void encrypt(char par){
 				for(down=up-1;down>=0;down--){
 				printf(" %d ",array[down]);
 				}
+					
+				
+			
 					printf("\n");
 			}
 			
@@ -133,7 +153,7 @@ void encrypt(char par){
 	}
 					
 void decryption(){
-	int length,start=0,last=6;
+	int length,start=0,last=7;
 	int i;
 	system("color 03");
 	system("cls");
@@ -149,7 +169,7 @@ void decryption(){
 	while(start != length){
 		decrypt(start,last);
 		start=last;
-		last=last+6;
+		last=last+7;
 	}
 	printf("\n");
 	system("pause");
@@ -158,12 +178,12 @@ void decryption(){
 	return;
 }
 void decrypt(int start,int last){
-	int i,value=0,j=5;
+	int i,value=0,j=6;
 	for(i=start;i<last;i++){
 		value=value+msg[i]*power(j);
 		j--;
 	}
-	j=5;
+	j=6;
 	compare(value);
 	return;
 }
@@ -178,7 +198,7 @@ int power(int pow){
 }
 void compare(int value){
 	int i;
-		for(i=10;i<37;i++){
+		for(i=0;i<88;i++){
 		if(i==value){
 			printf("%c",arr[i]);
 		}
@@ -207,6 +227,4 @@ void reset(){
 	    arr[18]='i',arr[19]='j',arr[20]='k',arr[21]='l',arr[22]='m',arr[23]='n',arr[24]='o',arr[25]='p',
 	    arr[26]='q',arr[27]='r',arr[28]='s',arr[29]='t',arr[30]='u',arr[31]='v',arr[32]='w',arr[33]='x',
 	    arr[34]='y',arr[35]='z',*/
-
-
 
